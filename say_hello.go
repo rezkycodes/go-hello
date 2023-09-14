3b83149d830da56e4fa42f234fe24390138d95d1
rezkypb <rezky.pradana.budihartono@gmail.com>
2023-09-14 11:22:48 +0800

    update say hello with params

--- say_hello.go
+++ say_hello.go
@@ -1,6 +1,6 @@
 package go_hello
 
-func SayHello(name string) string {
-	text := "Hello " + name
+func SayHello(name string, addr string) string {
+	text := "Hello : " + name + "Your address : " + addr
 	return text
 }
